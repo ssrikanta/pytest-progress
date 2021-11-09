183625ac780c0f7f098a98dbdca2a8ffdc0d586f
ssrikanta <santosh.srikanta@gmail.com>
2021-11-09 19:40:59 +0530

    config fixes and release 1.2.3

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import os
 import io
 
-from setuptools import setup
+import setuptools
 
-setup(name='pytest-progress',
+setuptools.setup(name='pytest-progress',
       version='1.2.3',
       description='pytest plugin for instant test progress status',
       long_description=io.open('README.rst', encoding='utf-8', errors='ignore').read(),
@@ -15,15 +15,14 @@ setup(name='pytest-progress',
       py_modules=['pytest_progress'],
       entry_points={'pytest': ['progress = pytest_progress']},
       install_requires=['pytest>=3.7'],
+      packages=setuptools.find_packages(),
       keywords='py.test pytest report',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Framework :: Pytest',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
-          'Operating System :: POSIX',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: OS Independent',
           'Topic :: Software Development :: Testing',
           'Topic :: Software Development :: Quality Assurance',
           'Topic :: Software Development :: Libraries',
